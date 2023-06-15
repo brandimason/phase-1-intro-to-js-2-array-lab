93e72aef08b8bfdec1bfbc90280d966f84b9094e
Brandi L Mason <masonbrandi66@gmail.com>
2023-06-14 21:43:11 -0600

    arrays lab completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    return [...cats, name];
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+
+function removeLastCat(){
+    const coolCats =[...cats];
+    coolCats.pop();
+    return coolCats;
+}
+
+function removeFirstCat(){
+    const coolCats =[...cats];
+    coolCats.shift();
+    return coolCats;
+}
